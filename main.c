92ecf2fd416c7e1729580232cfd57198318fa0da
Hoang Ta <tlghng@gmail.com>
2025-07-08 16:40:50 +0700

    Update convert functions (ARGB8888 to RGB888 + RGB666)
    1. Input image: __logo_img_dat_lvds (ARGB8888 color format, 1280x800)
    2. Add ARGB8888 to RGB888 convert function
    3. Add ARGB8888 to RGB666 convert function
    4. After running the execution file, it will save convert data in binary files (.raw)
       Notes: Users can use HxD to export to C format file if necessary.
    5. Define a SMALL_ARRAY_TEST macro to select a small array as input of conversion instead for an large image array.
    
    Signed-off-by: Hoang Ta <tlghng@gmail.com>

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 #include <string.h>
 #include <stdint.h>
 
+#define SMALL_ARRAY_TEST (0)
 
 extern const uint8_t __logo_img_dat_lvds[] __attribute__ ((aligned (256))); // 1280*800*4UL
 uint32_t __logo_img_dat_lvds_size = 1280*800*4UL;
 
-const uint8_t input[] __attribute__ ((aligned (256))) = {
+const uint8_t sample_array[] __attribute__ ((aligned (256))) = {
     0xf1,0xf2,0xf3,0xf4,
     0xf1,0xf2,0xf3,0xf4,
     0xf1,0xf2,0xf3,0xf4,
@@ -20,7 +21,7 @@ const uint8_t input[] __attribute__ ((aligned (256))) = {
     0xf1,0xf2,0xf3,0xf4,
 };
 
-uint8_t output[1280*900*4] __attribute__ ((aligned (256))); // 1280*800*4UL
+uint8_t output[1280*900*4] __attribute__ ((aligned (256))); // 1280*800*4UL = 4608000
 
 void print_array(const uint8_t * buffer, int size)
 {
@@ -37,16 +38,96 @@ void print_array(const uint8_t * buffer, int size)
     printf("\n");
 }
 
+size_t cvt_argb8888_rgb888(const uint8_t * input_image, size_t input_size, uint8_t *output_image, uint8_t is_32bit_pixel)
+{
+    size_t output_size = 0;
+    int i = 0, j = 0;
+
+    if((!input_image) || (!output_image)) {
+        printf("[%s] NULL parameters.\n", __func__);
+    }
+    /* Convert data */
+    for(; i < input_size; i+=4)
+    {
+        output_image[j++] = input_image[i];
+        output_image[j++] = input_image[i+1];
+        output_image[j++] = input_image[i+2];
+        
+        if(is_32bit_pixel)
+        {
+            output_image[j++] = 0x00; // any value
+        }    
+    }
+    output_size = j;
+    return output_size;
+}
+
+size_t cvt_argb8888_rgb666(const uint8_t * input_image, size_t input_size, uint8_t *output_image, uint8_t is_32bit_pixel)
+{
+    size_t output_size = 0;
+    int i = 0, j = 0;
+
+    if((!input_image) || (!output_image)) {
+        printf("[%s] NULL parameters.\n", __func__);
+    }
+    /* Convert data */
+    for(; i < input_size; i+=4)
+    {
+        output_image[j++] = input_image[i] & 0xFC;
+        output_image[j++] = input_image[i+1] & 0xFC;
+        output_image[j++] = input_image[i+2] & 0xFC;
+        
+        if(is_32bit_pixel)
+        {
+            output_image[j++] = 0x00; // any value
+        } 
+    }
+    output_size = j;
+    return output_size;
+}
+
+int saveBufferToFile(const uint8_t* buffer, size_t size, const char* filename)
+{
+    FILE *fp;
+    char saved_name[100] = "";
+    // const char *extension = ".bin";
+    size_t n = 0;
+    memset(saved_name, 0, 100);
+    
+    strcat(saved_name, filename);
+    // strcat(saved_name, extension);
+    
+    if(!(fp = fopen(saved_name, "wb"))) // Save as binay file
+    {
+        printf("[%s:%d] Error: Cannot open output file\n", __FILE__, __LINE__);
+        return (-1);
+    }
+    n = fwrite(buffer, 1 , size, fp);
+    fclose(fp);
+    printf("Save to %s file (%ld bytes)\n", saved_name, size);
+    return 0;
+}
 int main(int argc, char** argv)
 {
-
-    const char *dst = "output_rgb888.txt";
-    const uint8_t *p_input_image = __logo_img_dat_lvds;
-    uint8_t *p_output_image = output;
-    size_t input_size = __logo_img_dat_lvds_size;  //sizeof(input);
-    size_t output_size = 0; //sizeof(output);
+    const uint8_t *p_input_image;
+    uint8_t *p_output_image;
+    size_t input_size;
+    size_t output_size;
     char file_name[100];
     
+    
+#if (SMALL_ARRAY_TEST == 0)
+    p_input_image = __logo_img_dat_lvds;
+    p_output_image = output;
+    input_size = __logo_img_dat_lvds_size;  //sizeof(input);
+    output_size = 4608000;
+#else // (SMALL_ARRAY_TEST == 1)
+    p_input_image = sample_array;
+    input_size = sizeof(sample_array);
+    p_output_image = output;
+    output_size = sizeof(output);
+#endif
+
     if(argc >= 2)
     {
         printf("Input file: %s\n", argv[1]);
@@ -55,44 +136,32 @@ int main(int argc, char** argv)
 
 
     printf("Input Image (%ld bytes)\n", input_size);
-    print_array(p_input_image, input_size);
-    
-    /* Convert data */
-    int i = 0, j = 0;
-    for(; i < input_size; i+=4)
-    {
-        p_output_image[j] = p_input_image[i];
-        p_output_image[j+1] = p_input_image[i+1];
-        p_output_image[j+2] = p_input_image[i+2];
-        // p_input_image[j+3]; // skipped data
-        j += 3;
-    }
-    output_size = j;
-    printf("Output Image (%ld bytes)\n", output_size);
-    print_array(p_output_image, output_size);
+    // print_array(p_input_image, input_size);
+    saveBufferToFile(p_input_image, input_size, "argb8888.raw");
 
-    FILE *f_in;
-    if(!(f_in = fopen("argb8888.raw", "wb"))) // Save as binay file
-    {
-        printf("[%s:%d] Error: Cannot open output file\n", __FILE__, __LINE__);
-		return (-1);
-    }
-    
-    fwrite(p_input_image, 1 , input_size, f_in);
-    fclose(f_in);
-    printf("Save to argb8888.raw file\n");
+    memset(p_output_image, 0, output_size);
 
-    FILE *f_out;
-    if(!(f_out = fopen("rgb888.raw", "wb"))) // Save as binay file
-    {
-        printf("[%s:%d] Error: Cannot open output file\n", __FILE__, __LINE__);
-		return (-1);
-    }
-    fwrite(p_output_image, 1 , output_size, f_out);
-    fclose(f_out);
-    printf("Save to rgb888.raw file\n");
+    /* Convert ARGB8888 to RGB888 (24bit/pixel) */
+    output_size = cvt_argb8888_rgb888(p_input_image, input_size, p_output_image, 0);
+    saveBufferToFile(p_output_image, output_size, "rgb888_24bit.raw");
 
+    memset(p_output_image, 0, output_size);
     
+    /* Convert ARGB8888 to RGB888 (32bit/pixel) */
+    output_size = cvt_argb8888_rgb888(p_input_image, input_size, p_output_image, 1);
+    saveBufferToFile(p_output_image, output_size, "rgb888_32bit.raw");
+
+    memset(p_output_image, 0, output_size);
+    
+    /* Convert ARGB8888 to RGB666 (24bit/pixel) */
+    output_size = cvt_argb8888_rgb666(p_input_image, input_size, p_output_image, 0);
+    saveBufferToFile(p_output_image, output_size, "rgb666_24bit.raw");
+
+    memset(p_output_image, 0, output_size);
+    
+    /* Convert ARGB8888 to RGB666 (32bit/pixel) */
+    output_size = cvt_argb8888_rgb666(p_input_image, input_size, p_output_image, 1);
+    saveBufferToFile(p_output_image, output_size, "rgb666_32bit.raw");
 
     return 0;
 }
